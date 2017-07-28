3ec75357a263963bb9610dd4c9ee4ff5aa28fc6c
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-07-27 18:13:04 -0600

    j

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,13 @@
 $('document').ready(function() {
 
+  var moves = [];
+
   $('#Sumit').on("click", function() {
     event.preventDefault();
     var button = $("<button>");
     var text = $("#text").val();
+    button.text(text);
+
     $("#buttons").append(button);
 
   })
