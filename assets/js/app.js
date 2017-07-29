599dc98431f3e2d8c94c8923cdbddb6d6105196c
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-07-28 22:20:13 -0600

    buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,27 @@
 $('document').ready(function() {
   var moves = [];
 
+  function createButtons() {
+    $('#buttons').empty();
+    for (var i = 0; i < moves.length; i++) {
+      var newButton = $('<button>');
+      newButton.addClass("btn")
+      newButton.attr('moveName', moves[i]);
+      newButton.text(moves[i])
+      $('#buttons').append(newButton)
+    }
+  }
 
 
 
-  $("#addBtn").on("click", function(e) {
-    var newMove = $("#text").val();
-    var newButton = $("<button>");
-    newButton.text(newMove);
-    $("#buttons").append(newButton);
+  $("#add-move").on("click", function(event) {
+    event.preventDefault();
+    var move = $('#move-input').val().trim();
 
+    moves.push(move);
+    createButtons();
 
   });
+
 
 });
